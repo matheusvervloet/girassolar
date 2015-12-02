35b4d47fd3cffcb93d0ce516103c5843a8184c24
matheusvervloet <matheusvervloet@users.noreply.github.com>
2015-12-02 11:17:45 -0200

    Update lightsensor.py
    Coloquei a inicialização do sensor fora do while.

--- lightsensor.py
+++ lightsensor.py
@@ -1,8 +1,9 @@
 import mraa
 import time
-
+	
+lightSensor = mraa.Aio(0)
+	
 while 1:
 #Le o valor do light sensor e imprime o valor com um delay de 0.2 segundos
-	lightSensor = mraa.Aio(0)
 	print (lightSensor.read())
 	time.sleep(0.2)	
