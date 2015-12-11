9a40a9dedeb5f51da86e609ac57efefd31f005aa
matheusvervloet <matheusvervloet@users.noreply.github.com>
2015-12-11 13:32:47 -0200

    Versão final do código de controle do painel.

--- girassolar.py
+++ girassolar.py
@@ -12,10 +12,13 @@ DIR_CW = upmstepper.ULN200XA.DIR_CW
 DIR_CCW = upmstepper.ULN200XA.DIR_CCW
 
 lcd = upmlcd.Jhd1313m1(0, 0x3E, 0x62)
+lcd.setColor(0, 0, 0)
 
 relay = pyupm_grove.GroveRelay(6)
 
 lightSensor = mraa.Aio(3)
+
+solarPanel = mraa.Aio(0)
 
 def buttonISR(arg):
         global mode
@@ -43,45 +46,67 @@ def exitHandler():
         sys.exit(0)
 atexit.register(exitHandler)
 
-def writeLcd(text):
-        lcd.setCursor(0, 0)
+def writeLcd(text, line):
+        lcd.setCursor(line, 0)
         lcd.write(text)
 
 def step(steps):
         global position
-        if(position < 30 and position > -30):
-            if(steps > 0):
+        writeData('position.data', position)
+        if(position < 15 and steps >= 0):
                 stepper.setDirection(DIR_CW)
                 stepper.stepperSteps(steps)
                 position += steps
                 time.sleep(0.2)
-            else:
+        elif(position > -15 and steps < 0):
                 stepper.setDirection(DIR_CCW)
                 stepper.stepperSteps(-steps)
                 position += steps
                 time.sleep(0.2)
         else:
-            print 'Reached maximum steps.'
+            print 'Reached maximum steps (' + str(position) + ').'
+
+def readPanel():
+        reading = round(solarPanel.read()/205.0, 2)
+        writeLcd('Panel: ' + str(reading) + 'V ', 1)
+        writeData('panel.data', reading)
+        return reading
 
 def readLight():
         reading = lightSensor.read()
-        writeLcd('Light:' + str(reading))
+        writeLcd('Light: ' + str(reading), 0)
+        writeData('light.data', reading)
+        readPanel()
         return reading
+
+def standby():
+        global relay, lcd, stepper
+        writeData('status.data', 'off')
+        relay.off()
+        lcd.clear()
+        lcd.setColor(0, 0, 0)
+        step(-position)
+        stepper.release()
+
+
+def activate():
+        global relay, lcd
+        writeData('status.data', 'on')
+        relay.on()
+        lcd.setColor(255, 255, 255)
+
+def writeData(filename, data):
+        f = open(filename, 'w')
+        f.write(str(data))
+        f.close()
 
 while(1):
         if(mode == 0):
-                relay.off()
-                lcd.clear()
-                lcd.setColor(0, 0, 0)
-                step(-position)
+                standby()
                 while(mode == 0):
                         time.sleep(0.1)
 
-        relay.on()
-        lcd.setColor(255, 255, 255)
-        lcd.setCursor(1,0)
-        lcd.write('Panel output: 5V')
-
+        activate()
         firstValue = readLight()
 
         # enquanto a leitura do sensor de luz nao variar, ficamos parados
